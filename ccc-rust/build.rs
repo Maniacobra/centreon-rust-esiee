f23fefcf5dcaad4d62bbb99b83acb96d17865f98
Maniac0bra <76405497+Maniac0bra@users.noreply.github.com>
2024-03-05 16:06:57 +0100

    Server / Client gRPC test

--- ccc-rust/build.rs
+++ ccc-rust/build.rs
@@ -1,8 +1,4 @@
-// build.rs
-
-fn main() {
-    prost_build::Config::new()
-    .out_dir("src/proto") // Output directory for the generated code
-    .compile_protos(&["src/test.proto"], &["src"]) // Paths to the proto files and their include directories
-    .unwrap_or_else(|e| panic!("Failed to compile protos: {}", e));
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::compile_protos("proto/helloworld.proto")?;
+    Ok(())
 }
--- ccc-rust/src/main.rs
+++ ccc-rust/src/main.rs
@@ -1,10 +1,3 @@
-mod proto {
-    pub mod tutorial;
-}
-
 fn main() {
     println!("Hello, world!");
-    let mut test_person = proto::tutorial::Person::default();
-    test_person.name = String::from("Denis Bureau");
-    println!("Person: {:?}", test_person);
 }
